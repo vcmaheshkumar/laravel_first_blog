0d36f3ab92469f19f2022988beecb2e59844e97b
MAHESH KUMAR V C <info@vcmkcybersec.tech>
2021-08-19 14:10:56 +0530

    complete blog

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,100 @@
-HOME
+@extends('layouts.app')
+
+@section('content')
+    <div class="background-image grid grid-cols-1 m-auto">
+        <div class="flex text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-6 sm:m-auto w-4/5 block text-center">
+                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Do you want to become a developer?
+                </h1>
+                <a
+                    href="/blog"
+                    class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" width="700" alt="">
+        </div>
+
+        <div class="m-auto sm:m-auto text-left w-4/5 block">
+            <h2 class="text-4xl font-extrabold text-gray-600">
+                Struggling to be a better web developer?
+            </h2>
+
+            <p class="py-8 text-gray-500 text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit Obcaecati.
+            </p>
+
+            <p class="font-extrabold text-gray-600 text-xl pb-9">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime, hic at. Molestiae quisquam.
+            </p>
+
+            <a 
+                href="/blog"
+                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+                Find Out More
+            </a>
+        </div>
+    </div>
+
+    <div class="text-center p-15 bg-black text-white">
+        <h2 class="text-2xl pb-5 text-l">
+            I'm an expert in...
+        </h2>
+
+        <span class="font-extrabold block text-4xl py-1">
+            Ux Design
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Project Management
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Digital Stratergy
+        </span>
+        <span class="font-extrabold block text-4xl py-1">
+            Backend Development
+        </span>
+    </div>
+
+    <div class="text-center py-15">
+        <span class="uppercase text-s text-gray-400">
+            Blog
+        </span>
+
+        <h2 class="text-4xl font-bold py-10">
+            Recent Posts
+        </h2>
+
+        <p class="m-auto w-4/5 text-gray-500">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Deserunt nam dolorum nulla rem iure et, unde repudiandae. 
+            Inventore assumenda.
+        </p>
+    </div>
+
+    <div class="sm:grid grid-cols-2 w-4/5 m-auto">
+        <div class="flex bg-yellow-700 text-gray-100 pt-10">
+            <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
+                <span class="uppercase text-xs">
+                    PHP
+                </span>
+
+                <h3 class="text-xl font-bold py-10">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum officiis tenetur aliquam numquam libero a optio minima quam expedita deleniti. Cupiditate, dolor? Atque qui repellat vel quam quasi autem molestiae.
+                </h3>
+
+                <a href="" class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
+                    Find Out More
+                </a>
+            </div>
+        </div>
+        <div>
+            <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none font-sans">
     <div id="app">
-        <header class="bg-blue-900 py-6">
+        <header class="bg-gray-800 py-6">
             <div class="container mx-auto flex justify-between items-center px-6">
                 <div>
                     <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
@@ -25,6 +25,8 @@
                     </a>
                 </div>
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                    <a class="no-underline hover:underline" href="/">Home</a>
+                    <a class="no-underline hover:underline" href="/blog">Blog</a>
                     @guest
                         <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
                         @if (Route::has('register'))
@@ -45,7 +47,13 @@
             </div>
         </header>
 
-        @yield('content')
+        <div>
+            @yield('content')
+        </div>
+
+        <div>
+            @include('layouts.footer')
+        </div>
     </div>
 </body>
 </html>
